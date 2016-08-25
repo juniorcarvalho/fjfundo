0b3a691dbbdd4de6f519c0600a6c7d034880523c
Júnior Carvalho <joseadolfojr@gmail.com>
2016-08-25 04:20:47 -0300

    migrate

--- fjfundo/mensalidades/migrations/0006_auto_20160825_0435.py
+++ fjfundo/mensalidades/migrations/0006_auto_20160825_0435.py
@@ -111,7 +111,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='turma',
             name='id',
-            field=models.AutoField(auto_created=True, default=1, primary_key=True, serialize=False, verbose_name='ID'),
+            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
             preserve_default=False,
         ),
         migrations.DeleteModel(
