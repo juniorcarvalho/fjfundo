44b4743bee8998238c34389434c5cf7962d2cf51
Júnior Carvalho <joseadolfojr@gmail.com>
2016-08-25 04:24:00 -0300

    migrate

--- fjfundo/core/models.py
+++ fjfundo/core/models.py
@@ -32,7 +32,7 @@ class MyUser(PermissionsMixin, AbstractBaseUser):
         verbose_name=_('Email'),
         unique=True
     )
-    turma = models.ForeignKey(Turma)
+    turma = models.ForeignKey(Turma, null=True)
     cpf = models.CharField(verbose_name='cpf', max_length=14, null=True )
     identidade = models.CharField(verbose_name='identidade', max_length=20, null=True)
     nome = models.CharField(verbose_name='nome', max_length=50, null=True)
