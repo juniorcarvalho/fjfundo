d486128e341e74d6a3e20f9f856c00b6c073cde6
Júnior Carvalho <joseadolfojr@gmail.com>
2016-10-23 00:54:05 -0200

    allowed_hosts

--- fjfundo/settings.py
+++ fjfundo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 #ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
